fa2d8dc4b0c93fd10647350f428338657f27dbca
Nbalsh <brandon.bn.nelson@gmail.com>
2015-10-12 00:04:10 -0700

    Added Freeze, Edit Jump, Moving Platform
    Now can freeze frame, No double jump, moving or teleporting platform.
    Need to make it so stick better on platform

---
+++ Assets/scripts/FrozenPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrozenPlayer : MonoBehaviour {
+		
+		public GameObject deathParticles;
+		
+		public void Die()
+		{
+			Instantiate (deathParticles, transform.position, Quaternion.Euler (270, 0, 0));
+			transform.position = new Vector3(1000, 1000, 10000);
+			Destroy (this.gameObject, 0.1f);
+		}
+}
--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour {
 
@@ -12,24 +13,27 @@ public class PlayerController : MonoBehaviour {
 
 	bool isGrounded = false;
 
+	// old scripts
+	public GameObject frozenPlayer;
+	List<GameObject> frozenPlayers;
+	private Vector2 spawn;
+	public GameObject deathParticles;
+
 	// Use this for initialization
 	void Start () {
 		myBody = this.GetComponent<Rigidbody2D> ();
 		myTrans = this.transform;
 		tagGroundLeft = GameObject.Find (this.name + "/tag_Ground_Left").transform;
 		tagGroundRight = GameObject.Find (this.name + "/tag_Ground_Right").transform;
+
+		frozenPlayers = new List<GameObject>(5);
+		spawn = transform.position;
+
 	}
 	
 
 	void FixedUpdate () {
-
-		if (Physics2D.Linecast (myTrans.position, tagGroundLeft.position, playerMask)) {
-			isGrounded = Physics2D.Linecast (myTrans.position, tagGroundLeft.position, playerMask);
-			Debug.Log("isGroundedLeft");
-		} else if (Physics2D.Linecast (myTrans.position, tagGroundRight.position, playerMask)) {
-			isGrounded = Physics2D.Linecast (myTrans.position, tagGroundRight.position, playerMask);
-			Debug.Log("isGroundedRight");
-		}
+		isGrounded = isGroundedA ();
 
 #if !UNITY_ANDROID
 		Move (Input.GetAxisRaw("Horizontal"));
@@ -41,7 +45,19 @@ public class PlayerController : MonoBehaviour {
 	
 	}
 
+	public bool isGroundedA(){
+		if (Physics2D.Linecast (myTrans.position, tagGroundLeft.position, playerMask)) {
+			//Debug.Log("isGroundedLeft");
+			return Physics2D.Linecast (myTrans.position, tagGroundLeft.position, playerMask);
 
+
+		} else if (Physics2D.Linecast (myTrans.position, tagGroundRight.position, playerMask)) {
+			//Debug.Log("isGroundedRight");
+			return Physics2D.Linecast (myTrans.position, tagGroundRight.position, playerMask);
+		}
+		return false;
+
+	}
 	void Move(float inputHorizontal) {
 		Vector2 moveVel = myBody.velocity;
 		moveVel.x = inputHorizontal * speed;
@@ -50,7 +66,7 @@ public class PlayerController : MonoBehaviour {
 
 	public void Jump(){
 		if (isGrounded) {
-			Debug.Log("Jumping");
+			//Debug.Log("Jumping");
 			myBody.velocity += jumpVelocity * Vector2.up;
 		}
 		isGrounded = false;
@@ -58,5 +74,45 @@ public class PlayerController : MonoBehaviour {
 
 	public void StartMoving(float inputHorizontal){
 		hInput = inputHorizontal;
+	}
+
+	// old scripts
+	public void FreezeFrame()
+	{
+		if(frozenPlayers.Count < 5)
+		{
+			Vector3 positionToSpawn = transform.position;
+			transform.position = spawn;
+			GameObject frozenPlayerGO = Instantiate(frozenPlayer, positionToSpawn, Quaternion.Euler(0, 0, 0)) as GameObject;
+			frozenPlayers.Add(frozenPlayerGO);
+			Debug.Log("frozenPlayers.Count: " + frozenPlayers.Count);
+		}
+		else
+		{
+			frozenPlayers[0].GetComponent<FrozenPlayer>().Die();
+			frozenPlayers.RemoveAt(0);
+			Vector3 positionToSpawn = transform.position;
+			transform.position = spawn;
+			GameObject frozenPlayerGO = Instantiate(frozenPlayer, positionToSpawn, Quaternion.Euler(0, 0, 0)) as GameObject;
+			frozenPlayers.Add(frozenPlayerGO);
+		}
+	}
+
+
+	void Die()
+	{
+		Instantiate (deathParticles, transform.position, Quaternion.Euler (270, 0, 0));
+		transform.position = spawn;
+		
+		for(int i = 0; i < frozenPlayers.Count; ++i)
+		{
+			frozenPlayers[i].GetComponent<FrozenPlayer>().Die();
+		}
+		frozenPlayers.Clear();
+	}
+
+	public void SetCheckpoint(Vector2 position)
+	{
+		spawn = position;
 	}
 }
