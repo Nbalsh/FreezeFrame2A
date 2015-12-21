7c15e11c1bc1124ffe5e5825be81cbb225537f20
Nbalsh <brandon.bn.nelson@gmail.com>
2015-12-21 14:53:00 -0800

    Player now changes colour

---
+++ Assets/ChangePlayerColour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangePlayerColour : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            col.gameObject.GetComponent<SpriteRenderer>().color = this.GetComponent<ParticleSystem>().startColor;
+        }
+    }
+}
